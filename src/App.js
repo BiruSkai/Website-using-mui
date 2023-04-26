be43203fb94b4300fa6abc6aeea6c95583f705de
Nikolaus Albert <115626969+BiruSkai@users.noreply.github.com>
2023-04-26 15:34:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React, {useState, useEffect} from "react";
-import Popup from "./components/Popup";
+import React from 'react';
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import "./App.css";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
-  const [buttonPopup, setButtonPopup] = useState(false);  
-  const [timePopup, setTimePopup] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setTimePopup(true)
-    }, 2000)
-  }, [])
   return (
-    <div className="App">
-      <main>
-        <h1>React Popups</h1>
-        <br></br>
-        <button onClick= {() => setButtonPopup(true)}>Open Popup</button>
-      </main>
-      
-      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-        <h3>My Button Popup</h3>
-        <p>This is my triggered button Popup</p>
-      </Popup>
-      <Popup trigger={timePopup} setTrigger={setTimePopup}>
-        <h3>My Time Popup</h3>
-        <p>This is my triggered time Popup</p>
-      </Popup>
-    </div>
-  );
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/services" component={Services}/>
+        <Route path="/products" component={Products}/>
+        <Route path="/sign-up" component={SignUp}/>
+      </Switch>
+    </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
